cb572357defc2b82972283bf0e516c976ca0b22d
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2020-09-16 22:18:59 +0900

    add file for homework

--- partB/week1/practice_3.rkt
+++ partB/week1/practice_3.rkt
@@ -1,1 +1,16 @@
 #lang racket
+
+(define (my-if-bad x y z) (if x y z))
+
+
+(define (my-if x y z) (if x (y) (z)))
+
+(define (my-delay f)
+  (mcons #f f))
+
+(define (my-force th)
+  (if (mcar th)
+      (mcdr th)
+      (begin (set-mcar! th #t)
+             (set-mcdr! th ((mcdr th)))
+             (mcdr th))))
